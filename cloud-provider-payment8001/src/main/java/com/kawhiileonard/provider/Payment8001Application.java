d9d8781b9656333652e64c210fa5cb9d6cd4e351
Kawhiileonard <1657365982@qq.com>
2021-03-12 00:03:30 +0800

    初始化

---
+++ cloud-provider-payment8001/src/main/java/com/kawhiileonard/provider/Payment8001Application.java
@@ -0,0 +1,16 @@
+package com.kawhiileonard.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description 启动类
+ * @auth kawhiileonard
+ * @date 2021/3/11 23:13
+ */
+@SpringBootApplication
+public class Payment8001Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8001Application.class, args);
+    }
+}
